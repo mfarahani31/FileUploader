c26768dce553fdb9a5d301350834769edd0375db
Mohammad Farahani <farahani.dev@gmail.com>
2022-11-09 14:22:38 +0330

    Add: Integration test for getAll (for Sample)

--- src/test/java/com/example/informationmangement/InformationManagementApplicationTests.java
+++ src/test/java/com/example/informationmangement/InformationManagementApplicationTests.java
@@ -1,45 +1,32 @@
-//package com.example.informationmangement;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.fasterxml.jackson.databind.ObjectWriter;
-//import com.fasterxml.jackson.databind.SerializationFeature;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//@ExtendWith(SpringExtension.class)
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//@AutoConfigureMockMvc
-//class InformationManagementApplicationTests {
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @Test
-//    public void uploadFile() throws Exception {
-//        ObjectMapper mapper = new ObjectMapper();
-//        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
-//        ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
-//        String requestJson = ow.writeValueAsString(MotherObject.getAnyTransactionRequestDTO());
-//
-//
-//        mockMvc.perform(post("/api/v1/files/upload")
-//                        .contentType(MediaType.MULTIPART_FORM_DATA)
-//                        .content(requestJson)
-//                        .accept(MediaType.APPLICATION_JSON))
-//                .andDo(print())
-//                .andExpect(jsonPath("$.amount").value("100"))
-//                .andExpect(
-//                        status().isCreated());
-//    }
-//
-//}
+package com.example.informationmangement;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class InformationManagementApplicationTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetAllTransactionByUserId_whenIdNotExist_thenThrowsException() throws Exception {
+        mockMvc.perform(get("/api/v1/information/getAll"))
+                .andDo(print())
+                .andExpect(jsonPath("$").isEmpty())
+                .andExpect(
+                        status().isOk());
+    }
+
+}
