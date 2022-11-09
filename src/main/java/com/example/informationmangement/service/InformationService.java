4451c89ae7af0dcb88dfd355f515806d50c95fb9
Mohammad Farahani <farahani.dev@gmail.com>
2022-11-09 12:45:53 +0330

    Add: service for saving Information

--- src/main/java/com/example/informationmangement/service/InformationService.java
+++ src/main/java/com/example/informationmangement/service/InformationService.java
@@ -1,4 +1,31 @@
 package com.example.informationmangement.service;
 
+import com.example.informationmangement.helper.CSVHelper;
+import com.example.informationmangement.model.Information;
+import com.example.informationmangement.repository.InformationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
 public class InformationService {
+
+    private final InformationRepository informationRepository;
+
+    @Autowired
+    public InformationService(InformationRepository informationRepository) {
+        this.informationRepository = informationRepository;
+    }
+
+    public void save(MultipartFile file) {
+        try {
+            List<Information> informationList = CSVHelper.csvToInformations(file.getInputStream());
+            informationRepository.saveAll(informationList);
+        } catch (IOException e) {
+            throw new RuntimeException("fail to store csv data: " + e.getMessage());
+        }
+    }
 }
