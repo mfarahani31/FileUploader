14c3b6f6c2ee5ad98d9dbd968b64aac2cdfe1ed7
Mohammad Farahani <farahani.dev@gmail.com>
2022-11-09 20:20:54 +0330

    Add: unit test for sample

--- src/test/java/com/example/informationmangement/service/DefaultInformationServiceTest.java
+++ src/test/java/com/example/informationmangement/service/DefaultInformationServiceTest.java
@@ -1,15 +1,30 @@
 package com.example.informationmangement.service;
 
+import com.example.informationmangement.repository.InformationRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class DefaultInformationServiceTest {
 
+    @InjectMocks
+    DefaultInformationService informationService;
+
+    @Mock
+    InformationRepository informationRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
-    void getAll() {
-        when(informationRepository.findAll()).thenReturn(informations);
-
-        List<TransactionResponseDTO> actualTransactions = transactionService.getAllByUserId(1L);
-
-        assertEquals(transactions.size(), actualTransactions.size());
+    void testContext() {
+        assertNotNull(informationService);
+        assertNotNull(informationRepository);
     }
 }
