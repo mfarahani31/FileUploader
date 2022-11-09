e9a40f5c2200942cdaa538dbd280c061f0c66677
Mohammad Farahani <farahani.dev@gmail.com>
2022-11-09 12:23:58 +0330

    Add: annotations of repo

--- src/main/java/com/example/informationmangement/repository/InformationRepository.java
+++ src/main/java/com/example/informationmangement/repository/InformationRepository.java
@@ -1,4 +1,9 @@
 package com.example.informationmangement.repository;
 
-public interface InformationRepository {
+import com.example.informationmangement.model.Information;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InformationRepository extends JpaRepository<Information,Long> {
 }
