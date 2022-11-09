c1a289a906758e902345e0192f509bedec32b77d
Mohammad Farahani <farahani.dev@gmail.com>
2022-11-09 14:31:45 +0330

    Add: unit test for sample

--- src/test/java/com/example/informationmangement/service/DefaultInformationServiceTest.java
+++ src/test/java/com/example/informationmangement/service/DefaultInformationServiceTest.java
@@ -6,5 +6,10 @@ class DefaultInformationServiceTest {
 
     @Test
     void getAll() {
+        when(informationRepository.findAll()).thenReturn(informations);
+
+        List<TransactionResponseDTO> actualTransactions = transactionService.getAllByUserId(1L);
+
+        assertEquals(transactions.size(), actualTransactions.size());
     }
 }
